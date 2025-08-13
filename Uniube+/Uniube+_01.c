0089df6c9dede82741799c92dc7857b6a702b45b
KardelTheDwarf <49534906+KardelTheDwarf@users.noreply.github.com>
2025-08-13 10:55:04 -0300

    Uniube+_W01
    First week of Uniube+

---
+++ Uniube+/Uniube+_01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int x = 5, y = 10;
+
+    if (x > 3)
+    {
+        if (y < 10)
+        {
+            printf("A\n");
+        }
+        else
+        {
+        printf("B\n");}
+    }
+    return 0;
+
+}
+
+

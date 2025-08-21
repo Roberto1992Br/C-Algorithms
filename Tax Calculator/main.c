0d4274cc9445c402a71f315ebe1736e63b30c2ec
Roberto <49534906+Roberto1992Br@users.noreply.github.com>
2025-08-21 14:12:29 -0300

    Add files via upload
    11% Tax Calculator

---
+++ Tax Calculator/main.c
@@ -0,0 +1,31 @@
+//Aplicativo que desconta 11% em imposto do salário.
+
+#include <stdio.h>
+#include <locale.h>
+
+int main(void)
+{
+    setlocale(LC_ALL,"");
+    
+int Days;
+float Payment_B,Payment_L,Discount,Hours;
+
+//Frontend Questions
+
+printf("Digite o número de dias trabalhados: ");
+scanf("\n%d",&Days);
+
+printf("Digite quantas horas você trabalhou por dia: ");
+scanf("\n%f",&Hours);
+
+//Backend Maths
+
+Payment_B = (Hours*Days);
+Discount = (Payment_B*0.11);
+Payment_L = (Payment_B-Discount);
+
+printf("O seu salário bruto é de: R$ %.2F",Payment_B);
+printf("\nO valor descontado de impostos é: R$ %.2F",Discount);
+printf("\nO seu salário líquido é de: R$ %.2F",Payment_L);
+
+}
